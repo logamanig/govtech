b29524ed1442cdda7ec55def812eda736aa011fc
Logamani Gunasekaran <logamani@outlook.com>
2021-08-02 00:10:14 +0800

    typo fix for mem limit

--- challenge1/modules/fargate/r.ecs-task-definition.tf
+++ challenge1/modules/fargate/r.ecs-task-definition.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family                   = var.service_name
   cpu                      = var.container_cpu
-  memory                   = var.container_cpu
+  memory                   = var.container_memory
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
